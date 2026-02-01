ed02c81ad4059a64537e1e8e4f09f4c8b5c0b506
ritesh2004 <jadhavritesh2202@gmail.com>
2026-02-01 11:10:22 +0530

    Setup and custom hooks

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+     <div className="bg-blue-500 text-white p-6 rounded-lg">
+  Hello Tailwind!
+</div>
+)
+}
+
+export default App
